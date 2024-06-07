be30757d3fc46df22874ec547c6dc8e6b7ce7b13
Ajani Brown <ajani1brown.gmail.com>
2024-06-06 22:05:14 -0400

    first commit

---
+++ JavaScript Basics and Syntax/main.js
@@ -0,0 +1,65 @@
+//Understanding Data Types in JavaScript
+
+//Task 1: Declare variables to represent student information such as name, age, grade, and attendance status.
+let student;
+let age;
+let grade;
+let attendance;
+
+//test
+console.log(student);
+
+student= "kevin";
+console.log(student);
+
+//Task 2: Assign sample values to the student information variables.
+
+student= "Max"
+age= 16
+grade= 82
+attendance = true
+
+//Task 3: Display the student information using console.log() statements.
+
+console.log(student);
+console.log(age);
+console.log(grade);
+console.log(attendance);
+
+//Exploring JavaScript Operators
+
+//Task 1: Declare variables to store two numeric values for performing arithmetic operations.
+let num1
+let num2
+
+//Task 2: Assign sample numeric values to the variables declared in Task 1.
+
+num1 = 5
+num2 = 10
+
+//Task 3: Perform arithmetic operations on your numeric values using various operators and display the results.
+
+console.log("Sum:", num1 + num2);
+console.log("Difference:", num1 - num2);
+console.log("Product:", num1 * num2);
+console.log("Quotient:", num1 / num2);
+
+// Task 4: Explore assignment operators
+console.log("Updated num1:", num1 += num2);
+console.log("Updated num2:", num2 -= num1);
+
+// Task 5: Use comparison operators
+console.log("Is num1 equal to num2?", num1 == num2)
+console.log("Is num1 greater than num2?", num1 > num2);
+console.log("Is num1 not equal to num2?", num1 != num2);
+
+// Task 6: Apply logical operators
+const num3 = 7 ;
+const num4 = 7
+const num5 = 13
+const num6 = 9;
+let correct = true;
+let incorrect = false;
+
+console.log("are the numbers the same?", num3 === num4 == correct)
+console.log("are the numbers not the same?", num4 === num6 == incorrect)
